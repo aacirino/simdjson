83bf8099cd4f96fe717458490a23b6994e1dd59e
Ângelo Andrade Cirino <aacirino@gmail.com>
2022-01-07 14:06:54 -0300

    Support for modules and a better init script

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@ cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_ROOT:INTERNAL=$(brew --prefix)/opt/cmake/share/cmake \
   -DCMAKE_C_COMPILER:STRING=clang \
   -DCMAKE_CXX_COMPILER:STRING=clang++ \
-  -DCMAKE_INSTALL_INCLUDEDIR:PATH=include/simdjson
+  -DCMAKE_INSTALL_INCLUDEDIR:PATH=include/simdjson \
+  -DCMAKE_CXX_FLAGS="-Wno-unused-local-typedefs -std=c++20 -I$HOMEBREW_DIR/include -I$HOMEBREW_DIR/opt/llvm/include -I$HOMEBREW_DIR/opt/llvm/include/c++/v1 -march=native -isystem $HOMEBREW_DIR/opt/llvm//include/c++/v1/include -stdlib=libc++"
