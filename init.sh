ce8ed43489e3f185d78840979930a5492ef2ba41
Ângelo Andrade Cirino <aacirino@gmail.com>
2022-03-24 12:11:35 -0300

    Init script

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-setclang
+#setclang
 cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-  -DCMAKE_ROOT:INTERNAL=$(brew --prefix)/opt/cmake/share/cmake \
-  -DCMAKE_C_COMPILER:STRING=clang \
-  -DCMAKE_CXX_COMPILER:STRING=clang++ \
-  -DCMAKE_INSTALL_INCLUDEDIR:PATH=include/simdjson \
-  -DCMAKE_CXX_FLAGS="-Wno-unused-local-typedefs -std=c++20 -I$HOMEBREW_DIR/include -I$HOMEBREW_DIR/opt/llvm/include -I$HOMEBREW_DIR/opt/llvm/include/c++/v1 -march=native -isystem $HOMEBREW_DIR/opt/llvm//include/c++/v1/include -stdlib=libc++"
+  -D CMAKE_CXX_COMPILER_LAUNCHER=ccache \
+  -D CMAKE_ROOT:INTERNAL=$(brew --prefix)/opt/cmake/share/cmake \
+  -D CMAKE_C_COMPILER:STRING=clang \
+  -D CMAKE_CXX_COMPILER:STRING=clang++ \
+  -D CMAKE_INSTALL_INCLUDEDIR:PATH=include/simdjson \
+  -D CMAKE_CXX_FLAGS="-Wno-unused-local-typedefs -std=c++20 -I$HOMEBREW_DIR/include -I$HOMEBREW_DIR/opt/llvm/include -I$HOMEBREW_DIR/opt/llvm/include/c++/v1 -march=native -isystem $HOMEBREW_DIR/opt/llvm//include/c++/v1/include -stdlib=libc++"
